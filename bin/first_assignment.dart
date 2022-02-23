7935c8cb4ca163054b795b4b8e0341baf33b3924
Abhishek Rao <rawabhishek5@gmail.com>
2022-02-24 01:13:58 +0530

    Input data type conversion

--- bin/first_assignment.dart
+++ bin/first_assignment.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:io';
+import 'dart:convert';
 class Item{
   String? name;
   double? price;
@@ -8,7 +9,36 @@ class Item{
   double? salesTax;
   double? finalPrize;
 }
-
+//Check variable integer or not
+bool checkInt(var temp){
+    if((int.tryParse(temp)).toString()==temp){
+        return true;
+    }
+    else{
+      return false;
+    }
+}
+//Convert datatype of input
+dynamic convertDataType(var x){
+  if (double.tryParse(x) != null) {
+    return checkInt(x)?int.tryParse(x):double.tryParse(x);
+  }
+  else{
+    return x;
+  }
+}
 void main(){
-    
+  String flag="n";
+    do{
+       var name=stdin.readLineSync();
+       var x=stdin.readLineSync();
+       var y=stdin.readLineSync();
+       var z=stdin.readLineSync();
+       if(x!=null && y!=null && z!=null) {
+         var l,m,n;
+         l = convertDataType(x);
+         m = convertDataType(y);
+         n = convertDataType(z);
+       }
+    }while(flag=="y");
 }
--- test/test.dart
+++ test/test.dart
@@ -1,6 +1,18 @@
 import '../bin/first_assignment.dart';
 import "package:test/test.dart";
 void main() {
-
-
+  String test1="12.5";
+  String test2="10";
+  String test3="Bike";
+  group("Test input data type",(){
+    test("test1",(){
+      expect(convertDataType(test1).runtimeType,double);
+    });
+    test("test2",(){
+      expect(convertDataType(test2).runtimeType,int);
+    });
+    test("test3",(){
+      expect(convertDataType(test3).runtimeType,String);
+    });
+  });
 }
