11fe6717ea2ea5f2e04467f0cb7c8192c1a50dc1
Abhishek Rao <rawabhishek5@gmail.com>
2022-02-23 22:36:50 +0530

    Create Item class

--- bin/first_assignment.dart
+++ bin/first_assignment.dart
@@ -1,5 +1,13 @@
 import 'dart:collection';
 import 'dart:io';
+class Item{
+  String? name;
+  double? price;
+  int? quantity;
+  String? type;
+  double? salesTax;
+  double? finalPrize;
+}
 
 void main(){
     
